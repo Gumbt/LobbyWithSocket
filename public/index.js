b50d88b3e74669867e44f12ba24da25ccf5abd30
Gustavo Bonassa <09845549985@edu.udesc.br>
2019-07-31 17:56:37 -0300

    first commit

--- public/index.js
+++ public/index.js
@@ -3,5 +3,7 @@ var socket = io('http://localhost:3000');
 socket.emit('create', 'room1');
 
 socket.on('dispRooms', function(rooms) {
-    console.log(rooms)
+    for(room of rooms){
+        $('.servers').append('<div class="sv">'+room+'</div>')
+    }
 })
